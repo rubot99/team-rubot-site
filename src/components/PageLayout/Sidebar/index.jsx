87e9cfa94de38219493b2b8fdf9355ca210ebe6e
rubot99 <rubot99@gmail.com>
2021-08-12 21:49:23 +0100

    Updates to side bar

--- src/components/PageLayout/Sidebar/index.jsx
+++ src/components/PageLayout/Sidebar/index.jsx
@@ -21,32 +21,32 @@ const DomContent = () => (
     <div className={`${style.name} centerAlign`}>
       <div className={`${style.boxName} centerAlign`}>
         <h2>
-          Rolwin
+          Team
           {' '}
-          <span>Reevan</span>
+          <span>Rubot</span>
         </h2>
       </div>
-      <div className={`${style.badge} ${style.badgeGray}`}>Software Engineer</div>
+      { /*<div className={`${style.badge} ${style.badgeGray}`}>Team Rubot</div>*/}
       <div className="centerAlign box">
-        <a href={facebook} target="_blank" label="button" rel="noopener noreferrer"><FA name="facebook-f" /></a>
+        { /*<a href={facebook} target="_blank" label="button" rel="noopener noreferrer"><FA name="facebook-f" /></a>
         <a href={twitter} target="_blank" label="button" rel="noopener noreferrer"><FA name="twitter" /></a>
         <a href={github} target="_blank" label="button" rel="noopener noreferrer"><FA name="github" /></a>
-        <a href={instagram} target="_blank" label="button" rel="noopener noreferrer"><FA name="instagram" /></a>
+<a href={instagram} target="_blank" label="button" rel="noopener noreferrer"><FA name="instagram" /></a> */}
       </div>
       <ul className={`box ${style.badge} contactBlock`}>
-        <li className={`${style.contactBlockItem}`}>
+        { /*<li className={`${style.contactBlockItem}`}>
           <span>
             <FeatherIcon size="19" icon="calendar" />
             {' '}
           </span>
 &nbsp; &nbsp; May 9,1995
-        </li>
+</li>*/}
         <li className={`${style.contactBlockItem}`}>
           <span><FeatherIcon size="19" icon="map-pin" /></span>
           {' '}
-&nbsp; &nbsp; Bangalore, India
+&nbsp; &nbsp; Newcastle upon Tyne, UK
         </li>
-        <li className={`${style.contactBlockItem}`}>
+        {/*<li className={`${style.contactBlockItem}`}>
           <span><FeatherIcon size="19" icon="mail" /></span>
           {' '}
 &nbsp; &nbsp;
@@ -56,11 +56,11 @@ const DomContent = () => (
           >
             <span className={style.emailHider}>@</span>
           </a>
-        </li>
+</li>*/}
       </ul>
-      <div className={style.resumeDownload}>
+      {/*<div className={style.resumeDownload}>
         <a href="../resume.pdf" download target="_blank">Download CV</a>
-      </div>
+</div>*/}
     </div>
   </aside>
 );
