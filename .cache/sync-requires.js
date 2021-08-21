ebb45616c981988c8d6e110b3fbb69ec51e5773c
rubot99 <rubot99@gmail.com>
2021-08-21 19:29:53 +0100

    Reset blog

---
+++ .cache/sync-requires.js
@@ -0,0 +1,18 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\reube\\source\\repos\\n\\team-rubot-site\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-index-jsx": hot(preferDefault(require("C:\\Users\\reube\\source\\repos\\n\\team-rubot-site\\src\\pages\\404\\index.jsx"))),
+  "component---src-pages-blog-index-jsx": hot(preferDefault(require("C:\\Users\\reube\\source\\repos\\n\\team-rubot-site\\src\\pages\\blog\\index.jsx"))),
+  "component---src-pages-contact-index-jsx": hot(preferDefault(require("C:\\Users\\reube\\source\\repos\\n\\team-rubot-site\\src\\pages\\contact\\index.jsx"))),
+  "component---src-pages-index-jsx": hot(preferDefault(require("C:\\Users\\reube\\source\\repos\\n\\team-rubot-site\\src\\pages\\index.jsx"))),
+  "component---src-pages-resume-jsx": hot(preferDefault(require("C:\\Users\\reube\\source\\repos\\n\\team-rubot-site\\src\\pages\\resume.jsx"))),
+  "component---src-pages-tags-index-jsx": hot(preferDefault(require("C:\\Users\\reube\\source\\repos\\n\\team-rubot-site\\src\\pages\\tags\\index.jsx"))),
+  "component---src-templates-post-post-jsx": hot(preferDefault(require("C:\\Users\\reube\\source\\repos\\n\\team-rubot-site\\src\\templates\\post\\post.jsx"))),
+  "component---src-templates-tags-index-jsx": hot(preferDefault(require("C:\\Users\\reube\\source\\repos\\n\\team-rubot-site\\src\\templates\\tags\\index.jsx")))
+}
+
